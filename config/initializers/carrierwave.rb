a93d5cdcfb04cbf24095e5b2c02dec8b7e3d4060
Pierre Mesure <pierre.mesure@gmail.com>
2018-03-28 15:46:17 +0200

    Using AWS only if the env variables have been set

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,17 +11,21 @@ end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 #
-CarrierWave.configure do |config|
- config.fog_provider = 'fog/aws'                                           # required
- config.fog_credentials = {
-   provider:              'AWS',                                           # required
-   aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
-   aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-   region:                'eu-central-1',                                  # optional, defaults to 'us-east-1'
-   host:                  's3.eu-central-1.amazonaws.com',                 # optional, defaults to nil
- }
- config.storage = :fog
- config.fog_directory  = Rails.application.secrets.aws_bucket                                      # required
- config.fog_public     = false                                             # optional, defaults to true
- config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }  # optional, defaults to {}
+if Rails.application.secrets.aws_access_key_id.present?
+  require "carrierwave/storage/fog"
+
+  CarrierWave.configure do |config|
+   config.fog_provider = 'fog/aws'                                           # required
+   config.fog_credentials = {
+     provider:              'AWS',                                           # required
+     aws_access_key_id:     Rails.application.secrets.aws_access_key_id,     # required
+     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
+     region:                'eu-central-1',                                  # optional, defaults to 'us-east-1'
+     host:                  's3.eu-central-1.amazonaws.com',                 # optional, defaults to nil
+   }
+   config.storage = :fog
+   config.fog_directory  = Rails.application.secrets.aws_bucket                                      # required
+   config.fog_public     = false                                             # optional, defaults to true
+   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }  # optional, defaults to {}
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320094344) do
+ActiveRecord::Schema.define(version: 20180320135041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
